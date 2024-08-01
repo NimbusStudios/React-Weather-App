e8dfa51364c510b913f561d0504d3f6bd1ccff9d
NimbusStudios <nimbusstudios917@gmail.com>
2024-08-01 18:02:41 +0200

    updated layout with weather icons and font treatment.

--- weatherapp/src/components/WeatherInfo.js
+++ weatherapp/src/components/WeatherInfo.js
@@ -1,39 +1,16 @@
-
-// This is a functional component that displays weather information. It takes
-// two props: weatherData, which is an object containing weather data for a
-// location, and units, which is a string indicating the unit of temperature
-// (metric or imperial).
-
 import React from 'react';
 
-// We define the WeatherInfo component as a function that takes two props
 const WeatherInfo = ({ weatherData, units }) => {
-
-  // We return a JSX element that contains the weather information. The className
-  // property is used to apply CSS styles to the element.
   return (
     <div className="weather-info">
-
-
       <h2>{weatherData.name}</h2>
-
-  
+      <img src={weatherData.icon} alt={weatherData.weather[0].description} />
       <p>Temperature: {weatherData.main.temp}°{units === 'metric' ? 'C' : 'F'}</p>
-
-    
       <p>Humidity: {weatherData.main.humidity}%</p>
-
-     
       <p>Wind Speed: {weatherData.wind.speed} {units === 'metric' ? 'm/s' : 'mph'}</p>
-
-      {/* Add hourly and daily forecasts here */}
-
+      <p>Description: {weatherData.weather[0].description}</p>
     </div>
   );
 };
 
-// We export the WeatherInfo component so that it can be used in other parts of
-// the application.
 export default WeatherInfo;
-
-
--- weatherapp/src/services/weatherService.js
+++ weatherapp/src/services/weatherService.js
@@ -6,7 +6,10 @@ export const fetchWeatherData = async (location, units) => {
     const response = await fetch(`${API_URL}?lat=${location.lat}&lon=${location.lon}&units=${units}&appid=${API_KEY}`);
     if (!response.ok) throw new Error('Failed to fetch weather data');
     const data = await response.json();
-    return data;
+    return {
+      ...data,
+      icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    };
   } catch (error) {
     console.error(error);
     return null;
